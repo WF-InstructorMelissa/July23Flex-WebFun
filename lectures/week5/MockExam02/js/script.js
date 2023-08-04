0d80b61d7f2c26ee651379e76da56754ea09c0cb
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-08-03 21:12:54 -0400

    saving after mock2

---
+++ lectures/week5/MockExam02/js/script.js
@@ -0,0 +1,13 @@
+console.log('js connected')
+
+function watch() {
+    alert("Sorry, Live Watch is not available in your area")
+}
+
+// function breakingNews() {
+//     setTimeout(function() {
+//         let news = document.getElementById('hidden')
+//         news.style.display = 'flex'
+//     }, 2000)
+// }
+// breakingNews()
